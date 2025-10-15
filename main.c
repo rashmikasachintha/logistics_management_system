63ac5f40714539b32d141077379898d9742f406b
rashmikasachintha <rashmikasachintha02@gmail.com>
2025-10-15 10:33:11 +0530

    Added function remove city

--- main.c
+++ main.c
@@ -6,11 +6,15 @@ char cities[MAXCITY][50];
 int citycount=0;
 
 void addCity();
+void removeCity();
+void displayCities();
+
 
 int main()
 {
 
     addCity();
+    removeCity();
     return 0;
 }
 void addCity(){
@@ -36,4 +40,34 @@ void addCity(){
 
     }
 
+}
+void removeCity() {
+    if (citycount == 0) {
+        printf("No cities to remove.\n");
+        return;
+    }
+    displayCities();
+
+    int index;
+    printf("Enter index of city to remove: ");
+    scanf("%d", &index);
+
+    if (index < 0 || index >= citycount) {
+        printf("Invalid index.\n");
+        return;
+    }
+
+    for (int i = index; i < citycount - 1; i++) {
+        strcpy(cities[i], cities[i + 1]);
+    }
+
+    (citycount)--;
+    printf("City removed successfully.\n");
+}
+void displayCities(){
+    printf("INDEX\t\tCITY\n");
+    printf("-------------------\n");
+    for(int i=0;i<citycount;i++){
+        printf("%d\t\t%s\n",i,cities[i]);
+    }
 }
